83a3ab8daa97dc3eba7fd8ec5dd47ade8a5ecf05
Shami03 <ihtisham11051@gmail.com>
2020-10-21 01:14:49 +0500

    Frontend finished

--- index.js
+++ index.js
@@ -70,4 +70,31 @@ $(document).ready(function() {
     }
   })
   
+
+  // product qty 
+  let $qty_up = $(".qty .qty-up");
+  let $qty_down = $(".qty .qty-down");
+  // let $input = $(".qty .qty_input");
+
+  // click event on up button
+  $qty_up.click(function(e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+    if($input.val() >= 1 && $input.val() <= 9) {
+      $input.val(function(i, oldval) {
+        return ++oldval;
+      });
+    }
+  });
+
+  // click event on down button
+  $qty_down.click(function(e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+    if($input.val() > 1 && $input.val() <= 10) {
+      $input.val(function(i, oldval) {
+        return --oldval;
+      });
+    }
+  });
+
+
 });
